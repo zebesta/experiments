a872b08510f1184d61c39d9bc72e13da9adb0b57
zebesta <chris.sebesta@gmail.com>
2016-06-23 11:36:15 -0400

    Created recycle view of card views

--- app/src/main/java/com/example/chrissebesta/experiments/MainActivity.java
+++ app/src/main/java/com/example/chrissebesta/experiments/MainActivity.java
@@ -1,13 +1,27 @@
 package com.example.chrissebesta.experiments;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mRecyclerView = (RecyclerView) findViewById(R.id.recycle_view);
+        mRecyclerView.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        String[] myData = {"Hello", "This", "is a random", "string", "with lots of", "different data with different", "lengths", "to test the way", "recycle view works" };
+
+
+        mAdapter = new MyAdapter(myData);
+        mRecyclerView.setAdapter(mAdapter);
     }
 }
