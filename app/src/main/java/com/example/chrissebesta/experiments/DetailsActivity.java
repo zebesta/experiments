fb6049ffb6bc4b1987547149c0c9b99161d639dd
zebesta <chris.sebesta@gmail.com>
2016-07-01 14:25:51 -0400

    Upgraded SDK and gradle versioning, added comment for passing parcelable with fragment

--- app/src/main/java/com/example/chrissebesta/experiments/DetailsActivity.java
+++ app/src/main/java/com/example/chrissebesta/experiments/DetailsActivity.java
@@ -16,6 +16,17 @@ public class DetailsActivity extends AppCompatActivity {
         PlantData plantData = intent.getParcelableExtra(getString(R.string.plant_extra_key));
         DetailsActivityFragment df = new DetailsActivityFragment();
 
+        
+//        How to properly load the details activity fragment with the parcelable data - taken from the sunshine example
+//        Bundle arguments = new Bundle();
+//        arguments.putParcelable(DetailFragment.DETAIL_URI, getIntent().getData());
+//        DetailFragment df = new DetailFragment();
+//        df.setArguments(arguments);
+//
+//        getSupportFragmentManager().beginTransaction()
+//                .add(R.id.weather_detail_container, df)
+//                .commit();
+
 
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
