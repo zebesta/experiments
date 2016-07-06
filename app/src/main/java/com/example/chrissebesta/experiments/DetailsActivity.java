3efdeae66ab0c84c42df6afc543e2e35fe1e59c6
zebesta <chris.sebesta@gmail.com>
2016-07-06 16:26:48 -0400

    added name to app and name to details avtivity

--- app/src/main/java/com/example/chrissebesta/experiments/DetailsActivity.java
+++ app/src/main/java/com/example/chrissebesta/experiments/DetailsActivity.java
@@ -1,5 +1,6 @@
 package com.example.chrissebesta.experiments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -14,14 +15,14 @@ public class DetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
-        //Intent intent = getIntent();
+        Intent intent = getIntent();
         //String plantDataId = intent.getParcelableExtra(getString(R.string.plant_extra_key));
-        //PlantData plantData = intent.getParcelableExtra(getString(R.string.plant_extra_key));
+        PlantData plantData = intent.getParcelableExtra(getString(R.string.plant_extra_key));
         //DetailsActivityFragment df = new DetailsActivityFragment();
 
 //        How to properly load the details activity fragment with the parcelable data - taken from the sunshine example
         Bundle arguments = new Bundle();
-        arguments.putParcelable(getString(R.string.plant_extra_key), getIntent().getParcelableExtra(getString(R.string.plant_extra_key)));
+        arguments.putParcelable(getString(R.string.plant_extra_key), plantData);
         Log.d(LOG_TAG, "Adding arguments to the details fragment");
         DetailsActivityFragment df = new DetailsActivityFragment();
         df.setArguments(arguments);
@@ -38,6 +39,7 @@ public class DetailsActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle(plantData.getName());
     }
 
     public boolean onOptionsItemSelected(MenuItem item) {
